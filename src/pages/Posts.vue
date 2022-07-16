88b8a9d598e4b0710773171b88fec5d7a9cd6e26
donaldxdonald <mzhlovefama@gmail.com>
2022-07-17 00:53:25 +0800

    style: update style of post list

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -11,19 +11,24 @@ const postBlocks: PostBlock[] = [
 </script>
 
 <template>
-  <div class="w-full h-full relative flex flex-col items-center overflow-y-auto overflow-x-hidden">
-    <a
-      class="cursor-pointer mt-10 text-xl text-theme font-italic"
-      href="/"
-    >DonaldxDonald</a>
-    <div class="max-w-lg main-layout flex flex-col items-center justify-center">
+  <div class="main-layout max-w-xl mx-auto h-full relative overflow-y-auto overflow-x-hidden">
+    <header class="mt-10 px-6 flex items-center justify-between text-theme">
+      <a
+        class="cursor-pointer text-2xl font-italic"
+        href="/"
+      >DonaldxDonald</a>
+      <a href="./feed.xml">
+        <mdi-rss class="mx-1" />
+      </a>
+    </header>
+    <div class="flex flex-col items-center justify-center">
       <div
         v-for="(block, index) in postBlocks"
         :key="index"
-        class="posts-block mt-5 w-full"
+        class="posts-block mt-5"
       >
-        <div class="px-4 py-2 mb-5 text-theme">
-          <span class="block-title text-2xl font-sans">{{ block.label }}</span>
+        <div class="px-4 py-2 mb-5 bg-theme/10 text-theme">
+          <span class="block-title text-xl font-sans">{{ block.label }}</span>
         </div>
         <div class="block-body my-5">
           <PostList
